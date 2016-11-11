c8c120f74a1f1c6034bad13f464f545df22a0681
mattKsp <brian7android@gmail.com>
2016-11-11 02:21:55 +0000

    end buttons vars and get

--- Main/Board/liveStrip_A/buttons.ino
+++ Main/Board/liveStrip_A/buttons.ino
@@ -3,22 +3,159 @@
 void setupButton() {
   //called from setup
   
-  //..do setup for each button
-  
-  setupEndButtonInterrupts();
+  //..do setup for each end button
+  for (int i = 0; i < _endButtonTotal; i++) {
+    pinMode(_endButtonPin[i], INPUT_PULLUP);  //activate internal pullup resistor on pin
+    _endButton[i].attach(_endButtonPin[i]);   //attach pin to empty bounce button
+    _endButton[i].debounceTime = 5;           //unsigned long (5ms)
+  }
+
+  //do setup for each knob button
+  for (int i = 0; i < _knobTotal; i++) {
+    pinMode(_knobButtonPin[i], INPUT_PULLUP); //activate internal pullup resistor on pin
+    _knobButton[i].attach(_knobButtonPin[i]); //attach pin to empty bounce button
+    _knobButton[i].debounceTime = 5;          //unsigned long (5ms)
+  }
+
+  //setup other button
+  pinMode(_otherButtonPin, INPUT_PULLUP);     //activate internal pullup resistor on pin
+  _otherButton.attach(_otherButtonPin);       //attach pin to empty bounce button
+  _otherButton.debounceTime = 5;              //unsigned long (5ms)
+
+  //setupEndButtonInterrupts(); //OVERKILL
 }
 
+void endButtonGet() {
+  //sub-loop. reads all the end buttons
+  for (int i = 0; i < _endButtonTotal; i++) {
+    boolean hasChanged = _endButton[i].update();  //Bounce buttons keep internal track of whether any change has occured since last time
+    
+    if (hasChanged) {
+      
+      if (_endButtonBehaviour[i] == 0) {
+        //momentary
+        
+        if (_endButton[i].fell()) {
+          if (_endButtonType[i]== 0) {
+            //send MIDI note On
+            usbMIDI.sendNoteOn(_endButtonMidiNote[i], _endButtonMidiOnValue[i], _midiChannel); //note, val (127), midi channel
+            //usbMIDI.send_now();
+            //set flag for messaget to show
+          } else if (_endButtonType[i] == 1) {
+            //send MIDI CC  - just to send CC max on fall
+            usbMIDI.sendControlChange(_endButtonMidiCC[i], _endButtonMidiOnValue[i], _midiChannel);  //control, value, channel
+            //usbMIDI.send_now();
+            //flashMessage
+          }
+        } //END .fell
+        else if (_endButton[i].rose()) { 
+          if (_endButtonType[i] == 0) {
+            //send MIDI note Off
+            usbMIDI.sendNoteOff(_endButtonMidiNote[i], _endButtonMidiOffValue[i], _midiChannel); //note, val (0), midi channel - or would better vals be either 0/64 or 65/127, smaller larger comparisons or somit like that?
+            //usbMIDI.send_now();
+            //flashMessage
+          }
+        } //END .rose
+        
+      } //END momentary
+      else if (_endButtonBehaviour[i] == 1) {
+        //toggle
+        
+        //need for boolean to track on/off state and/or taking into account external input/bounce-back from the sequencer
+        if (_endButton[i].fell()) {
+          if (_endButtonToggleState[i]) {
+            //send Off
+            if (_endButtonType[i] == 0) {
+              //send MIDI note Off
+              usbMIDI.sendNoteOff(_endButtonMidiNote[i], _endButtonMidiOffValue[i], _midiChannel); //note, val (0), midi channel - or would better vals be either 0/64 or 65/127, smaller larger comparisons or somit like that?
+              //usbMIDI.send_now();
+              //flashMessage
+            }
+          } else if {
+            //send On
+            if (_endButtonType[i]== 0) {
+              //send MIDI note On
+              usbMIDI.sendNoteOn(_endButtonMidiNote[i], _endButtonMidiOnValue[i], _midiChannel); //note, val (127), midi channel
+              //usbMIDI.send_now();
+              //set flag for messaget to show
+            } else if (_endButtonType[i] == 1) {
+              //send MIDI CC  - just to send CC max on fall
+              usbMIDI.sendControlChange(_endButtonMidiCC[i], _endButtonMidiOnValue[i], _midiChannel);  //control, value, channel
+              //usbMIDI.send_now();
+              //flashMessage
+            }
+          } //END send On
+        } //END .fell
+        else if (_endButton[i].rose()) { 
+          //
+        } //END .rose
+        
+        _endButtonToggleState[i] = !_endButtonToggleState[i]; //flip state for next time  //can be adjusted elswhere dependant on metering source
+      } //END toggle
+      
+    } //END hasChanged
+
+    //time tracking? ..need to track long hold on buttons
+    //could just change a flag on fall and rise, 
+    //but unless using iterrupts cannot garuntee that it won't 
+    //miss one of them and get stuck in the wrong position
+    
+/*    unsigned long buttonPressTimeStamp;
+    buttonPressTimeStamp = millis();
+    if  ( buttonState == 1 ) {
+      if ( millis() - buttonPressTimeStamp >= 500 ) {
+         buttonPressTimeStamp = millis();
+         if ( ledState == HIGH ) ledState = LOW;
+         else if ( ledState == LOW ) ledState = HIGH;
+         digitalWrite(LED_PIN, ledState );
+        Serial.println("Retriggering button");
+      }
+    }
+*/    
+    if (_endButtonBehaviour[i] == 0) {
+        //if momentary..
+    }
+    else if (_endButtonBehaviour[i] == 1) {
+        //if toggle..
+        //_endButtonToggleState[i]
+    }
+  
+  } //END main for loop
+} //END endButtonGet
+
+void knobButtonGet() {
+  //sub-loop. reads all the knob buttons (if used)
+  for (int i = 0; i < _knobTotal; i++) {
+    
+  } //END main for loop
+} //END knobButtonGet
+
+void otherButtonGet() {
+  //sub-loop. reads any other buttons
+  //check we are in menu mode first
+  if (_menuMode) {
+    boolean hasChanged = _otherButton.update();  //Bounce buttons keep internal track of whether any change has occured since last time
+    if (hasChanged) {
+      if (_otherbutton.read()) {
+        //
+      }
+    }
+  }
+}
+
+/*    ----------------OVERKILL-----------------------------
 //attachInterrupt(interruptPin, function, CHANGE);
 void setupEndButtonInterrupts() {
-  attachInterrupt(_endButtonPin[0], interruptEB0, CHANGE);  //FALLING, RISING, CHANGE
-  attachInterrupt(_endButtonPin[1], interruptEB1, CHANGE);
-  attachInterrupt(_endButtonPin[2], interruptEB2, CHANGE);
-  attachInterrupt(_endButtonPin[3], interruptEB3, CHANGE);
-  attachInterrupt(_endButtonPin[4], interruptEB4, CHANGE);
+  attachInterrupt(_endButton[0], interruptEB0, CHANGE);  //FALLING, RISING, CHANGE
+  attachInterrupt(_endButton[1], interruptEB1, CHANGE);
+  attachInterrupt(_endButton[2], interruptEB2, CHANGE);
+  attachInterrupt(_endButton[3], interruptEB3, CHANGE);
+  attachInterrupt(_endButton[4], interruptEB4, CHANGE);
 }
 
-void interruptEB0() {  }
+void interruptEB0() { _endButton[i].update(); }
 void interruptEB1() {  }
 void interruptEB2() {  }
 void interruptEB3() {  }
 void interruptEB4() {  }
+*/
--- Main/Board/liveStrip_A/liveStrip_A.ino
+++ Main/Board/liveStrip_A/liveStrip_A.ino
@@ -41,6 +41,7 @@ const String _progVers = "0.03";
 boolean _debug = true;
 const int _mainLoopDelay = 0;   //just in case
 int _orientation = 0;           //current orientation. 0-359  Can rotate faders to any angle.
+boolean _menuMode = false;      //flag for when we switch into menu mode
 
 /*----------------------------knob----------------------------*/
 const int _knobTotal = 8;                                             //total knobs on device (default=8)
@@ -48,23 +49,42 @@ lsRotaryEncoder _knob[_knobTotal];                                    //call bla
 const int _knobPinA[_knobTotal] = { 0, 2, 4, 6, 8, 10, 12, 15 };      //Teensy 3.2 pin assignments (encoders need 2 pins each). interrupt pins
 const int _knobPinB[_knobTotal] = { 1, 3, 5, 7, 9, 11, 14, 16 };      //miss out pin 13
 int _knobMidiCC[_knobTotal] = { 22, 23, 24, 25, 26, 27, 28, 29 };     //MIDI CC values
-int _knobType[_knobTotal] = { 0 };                                    //0=fixed rotary, 1=endless rotary, 2=pan, etc.
+int _knobBehaviour[_knobTotal] = { 0 };                               //0=fixed rotary, 1=endless rotary, 2=pan, etc.
 float _knobLimit[2][_knobTotal] = { {2.0}, {8.0} };                   //0-1 Start and end of simulated rotary limits. eg. like a real desk pot - used by type 0
 volatile float _knobRange[2][_knobTotal] = { {0.0}, {1.0} };          //0-1 Start and end of restricted range within limits. eg. like MIDI mapping in a DAW - used by type 0, 1, 2
-boolean _knobAffectSpeed[_knobTotal] = { false };                     //whether the knob is currently using this speed multiplier. eg. switch between slow/fast by clicking button.
-volatile float _knobSpeed[_knobTotal] = { 1.0 };                        //speed multiplier
+volatile float _knobCenter[_knobTotal] = { 0.5 };                     //we can move the center point.. hmm.. have to make sure it stays inside knob range
+boolean _knobSpeedAltInUse[_knobTotal] = { false };                   //whether the knob is currently using this speed multiplier. eg. switch between slow/fast by clicking button.
+volatile float _knobSpeed[2][_knobTotal] = { {1.0}, {0.25} };         //normal speed multiplier and alternate speed multiplier
 
 /*----------------------------button----------------------------*/
 const int _endButtonTotal = 5;                                        //somewhere between 3 and 5 buttons at the end
-const int _endButtonPin[_endButtonTotal] = { 17, 18, 19, 20, 21 };            //Teensy 3.2 pin assignments. interrupt pins
+Bounce _endButton[_endButtonTotal];                                   //de-bounced buttons
+Bounce _knobButton[_knobTotal];                                       //knob buttons
+Bounce _otherButton;                                                  //extra button for system stuff
+const int _endButtonPin[_endButtonTotal] = { 17, 18, 19, 20, 21 };          //Teensy 3.2 pin assignments. interrupt pins
 const int _knobButtonPin[_knobTotal] = { 22, 23, 24, 25, 26, 27, 28, 29 };  //if used knob buttons will be push switches integrated into the shaft of the encoder. this may be changed to capacitive touch
-const int _otherButtonPin[1] = { 30 };                                //extras, not worth running a total for.. (used for system, does not send MIDI)
+const int _otherButtonPin = 30;                                             //extra, only need one to start with.. accessible when in menu mode (used for system, does not send MIDI)
 int _endButtonMidiNote[_endButtonTotal] = { 0, 1, 2, 3, 4 };                //MIDI note values for end buttons
 int _knobButtonMidiNote[_knobTotal] = {10, 11, 12, 13, 14, 15, 16, 17 };    //MIDI note values for knob buttons (if used)
+//int _endButtonTriggerStyle[_knobTotal] = { 2 };                             //when sending MIDI messages, send on.. a)rose, b)fell, or c)change (.read)
+int _endButtonMidiCC[_knobTotal] = { 85, 86, 87, 88, 89 };            //MIDI CC values for end buttons (90 is also available in this range set)
+int _endButtonType[_knobTotal] = { 0 };                               //0=MIDI note 1=MIDI CC
+int _endButtonBehaviour[_knobTotal] = { 0 };                          //0=momentary 1=toggle
+boolean _endButtonToggleState[_knobTotal] = { false };                //toggle state - false=off true=on    //this can also be adjusted dependant on metering source
+int _endButtonMidiOnValue[_knobTotal] = { 127 };                      //0-127 - value to send out in MIDI message
+int _endButtonMidiOffValue[_knobTotal] = { 0 };                       //0-127 - ..
 
 /*----------------------------display----------------------------*/
-int _meteringSource = 2;  //when displaying knob values, do we a) take the value straight from the knob, b) wait for the bounce-back from the sequencer, or c) compare and use both. (this could be usefull)
-float _displayBrightnessGlobal = 1.0;                                   //global brightness for displays/LEDs
+int _knobMeteringSource = 2;                                          //value source for metering
+/* when displaying knob values, do we..
+ * a) take the value straight from the knob, 
+ * b) wait for the bounce-back from the sequencer, or 
+ * c) compare and use both. 
+ * (this could be usefull)
+ * ..similar to local control on a synth
+*/
+int _endButtonMeteringSource = 2;                                     //..same for end buttons
+float _displayBrightnessGlobal = 1.0;                                 //global brightness for displays/LEDs
 /*-----------display - screen------------*/
 
 /*-----------display - led------------*/
@@ -73,7 +93,7 @@ const int _knobLEDRingBTotal = 16;                                    //32
 
 /*----------------------------communication----------------------------*/
 /*-----------communication - MIDI------------*/
-int _defaultMIDIChannel = 1;                                          //the default MIDI channel
+const int _midiChannel = 1;                                          //the default MIDI channel - 0=global
 
 
 /*----------------------------MAIN----------------------------*/
